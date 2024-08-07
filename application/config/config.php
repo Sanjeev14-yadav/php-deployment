1a59ab5d150bf78f9c770ad1e2ea744ae082f4c5
Sanjeev14-yadav <sanjeev.y@virtualglobetechnology.com>
2024-08-07 18:53:02 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/crud/';
+$config['base_url'] = 'http://54.91.102.226/';
 
 /*
 |--------------------------------------------------------------------------
